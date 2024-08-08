e19fd509a156d81f273daa4b7c4aa77d2309d5dd
winston <zhen_wang_work@163.com>
2024-08-08 17:56:08 +0800

    变更sql

--- models/jaffle_shop/test_orders.sql
+++ models/jaffle_shop/test_orders.sql
@@ -1,1 +1,5 @@
-select * from raw_dbt.jaffle_shop.orders
+select  customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+ from raw_dbt.jaffle_shop.orders
